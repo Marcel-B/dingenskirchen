af9f36eaa1a6031daba04fb4078350beec084526
Marcel Benders <marcel.benders@netto-online.de>
2022-02-18 19:54:45 +0100

    Projekt umgezogen

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -13,41 +13,17 @@ con = sqlite3.connect('haushaltsbuch.db')
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/DcKampagnen/promotions/<id>/historie', methods = ['GET'])
-def historie(id):
-    data=[{
-        "aktion": "12345670",
-        "benutzer": "NETTO\\Bendersm",
-        "datum": datetime.datetime.now().isoformat(),
-        "daten": {
-            "foo": "bar",
-            "Position": 2
-        }
-    }]
-    response=jsonify(data)
-    return response
-
 @app.route('/haushaltsbuch/posten', methods = ['POST'])
 def posten_new():
     content = request.get_json()
     print(content)
     return "OK", status.HTTP_200_OK
 
-@app.route('/DcKampagnen/promotions', methods = ['POST'])
-def promotions():
-    data=[{
-        "id": 12345670,
-        "name": "netto\\Bendersm"
-    }]
-    response=jsonify(data)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     cur = con.cursor()
     # Create table
-    cur.execute('''CREATE TABLE buchungen 
+    cur.execute('''CREATE TABLE IF NOT EXISTS Buchungen
                    (date text, trans text, symbol text, qty real, price real)''')
 
     con.commit()
