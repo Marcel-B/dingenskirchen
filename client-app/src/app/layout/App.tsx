9439783257b60db125edc891ad948e932e31f127
Marcel Benders <marcel.benders@netto-online.de>
2022-02-23 08:46:55 +0100

    Error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,46 +1,48 @@
-import React, {Fragment} from 'react';
-import {Container} from 'semantic-ui-react';
-import {NavBar} from './NavBar';
-import {observer} from 'mobx-react-lite';
+import React, { Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import { NavBar } from './NavBar';
+import { observer } from 'mobx-react-lite';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
-import {Route, Switch, useLocation} from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from "../../features/activities/errors/TestError";
-import {ToastContainer} from "react-toastify";
-import NotFound from "../../features/activities/errors/NotFound";
+import TestErrors from '../../features/activities/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/activities/errors/NotFound';
+import ServerError from '../../features/activities/errors/ServerError';
 
 const App = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <>
-            <ToastContainer position={`bottom-right`} hideProgressBar/>
-            <Route exact path={`/`} component={HomePage}/>
-            <Route
-                path={`/(.+)`}
-                render={() => (
-                    <Fragment>
-                        <NavBar/>
-                        <Container style={{marginTop: '7em'}}>
-                            <Switch>
-                                <Route path={`/errors`} component={TestErrors}/>
-                                <Route exact path={`/activities`} component={ActivityDashboard}/>
-                                <Route path={`/activities/:id`} component={ActivityDetails}/>
-                                <Route
-                                    key={location.key}
-                                    path={[`/createActivity`, `/manage/:id`]}
-                                    component={ActivityForm}
-                                />
-                                <Route component={NotFound}/>
-                            </Switch>
-                        </Container>
-                    </Fragment>
-                )}
-            />
-        </>
-    );
+  return (
+    <>
+      <ToastContainer position={`bottom-right`} hideProgressBar />
+      <Route exact path={`/`} component={HomePage} />
+      <Route
+        path={`/(.+)`}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Switch>
+                <Route exact path={`/activities`} component={ActivityDashboard} />
+                <Route path={`/activities/:id`} component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={[`/createActivity`, `/manage/:id`]}
+                  component={ActivityForm}
+                />
+                <Route path={`/errors`} component={TestErrors} />
+                <Route path={`/server-error`} component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
+            </Container>
+          </Fragment>
+        )}
+      />
+    </>
+  );
 };
 
 export default observer(App);
